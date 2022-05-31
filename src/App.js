5cdaa659a50ad56812bd2798c72f1413fed49071
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-31 11:44:16 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import RequestsDataComponent, {RequestsDataTest} from "./noncomponents/RequestsDataComponent";
-import NavApp from "./nav/NavApp";
 import ScrollViewApp from "./scrollview/ScrollViewApp";
 import NavigationComponent from "./components/NavigationComponent";
 import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
