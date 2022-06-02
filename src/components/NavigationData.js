08ed58a47d08cd8329f33ff653eaae5633aa3d8b
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-02 09:36:08 +0300

    update NavigationData.js file

--- src/components/NavigationData.js
+++ src/components/NavigationData.js
@@ -4,34 +4,23 @@ export const REQUESTLOGSTYPES = [{
 	expanded: true,
 	items: [{
 		id: '1_1',
-		text: 'ALL LOGS',
-		title: 'All Enterprise Service Bus Logs',
-		description: 'This is a combination of request log made both to the ATM Bridge and Members portal and their corresponding response',
+		text: 'ATM Bridge Logs',
+		title: 'ATM Bridge Logs',
+		description: 'These are request logs made only to the ATM Bridge and their corresponding response',
 		selected: true,
 		options: [{
 			id: '1_1_1',
-			text: 'All Enterprise Service Bus Logs'
+			text: 'ATM Bridge Log'
 		}]
 	},
 		{
 			id: '1_2',
-			text: 'ATM Bridge Logs',
-			title: 'ATM Bridge Logs',
-			description: 'These are request logs made only to the ATM Bridge and their corresponding response',
-			options: [{
-				id: '1_2_2',
-				text: 'ATM Bridge Log'
-			}]
-		},
-		{
-			id: '1_3',
 			text: 'Members Portal Logs',
 			title: 'Members Portal Request Logs',
 			description: 'These are request logs made only to the Members and their corresponding response',
 			options: [{
-				id: '1_3_1',
+				id: '1_2_2',
 				text: 'Members Portal Request Logs',
-				description: 'Kuala Lumpur, officially the Federal Territory of Kuala Lumpur, or commonly KL, is the largest city of Malaysia. It is located in Klang Valley.'
 			}]
 		}
 	]
