414bbf0ad4df9658e2fb5723ff08260813054888
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-02 13:05:19 +0300

    add and update profile.js file

---
+++ src/components/profile/profile.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './profile.css';
+import Form from 'devextreme-react/form';
+
+export default function Profile() {
+
+  const employee = {
+    ID: 7,
+    Email: 'John@example.com',
+    FirstName: 'John',
+    LastName: 'Karani',
+    Position: 'Dev',
+  };
+
+  return (
+    <React.Fragment>
+      <h2 className={'content-block'}>Profile</h2>
+
+      <div className={'content-block dx-card responsive-paddings'}>
+        <Form
+          id={'form'}
+          defaultFormData={employee}
+          labelLocation={'top'}
+          colCountByScreen={colCountByScreen}
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+
+const colCountByScreen = {
+  xs: 1,
+  sm: 2,
+  md: 3,
+  lg: 4
+};
